f1f9269afc5107de5a8e6781094ab1167143d95b
Chonlasit <chonlasit.s@arise.tech>
2023-01-12 10:43:16 +0700

    add interface

--- repository/user.go
+++ repository/user.go
@@ -4,13 +4,16 @@ import (
 	"parktify/models"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type UserRepository interface {
-	CreateUser(user *models.User) error
+	CreateUser(user models.User) error
 	GetUserByEmail(email string) (*models.User, error)
-	GetUsers() (users []*models.User, err error)
+	GetUsers() (users []models.User, err error)
+	UpdateUser(user models.User) error
+	DeleteUser(uid uuid.UUID) error
 }
 
 type userRepository struct {
@@ -22,7 +25,7 @@ func NewUserRepository(db *gorm.DB, redis *redis.Client) UserRepository {
 	return &userRepository{db, redis}
 }
 
-func (r *userRepository) CreateUser(user *models.User) error {
+func (r *userRepository) CreateUser(user models.User) error {
 
 	return r.db.Create(user).Error
 }
@@ -32,7 +35,7 @@ func (r *userRepository) GetUserByEmail(email string) (*models.User, error) {
 	return nil, nil
 }
 
-func (r *userRepository) GetUsers() (users []*models.User, err error) {
+func (r *userRepository) GetUsers() (users []models.User, err error) {
 
 	err = r.db.Find(&users).Error
 	if err != nil {
@@ -40,4 +43,12 @@ func (r *userRepository) GetUsers() (users []*models.User, err error) {
 	}
 
 	return users, err
+}
+
+func (r *userRepository) UpdateUser(user models.User) error {
+	return r.db.Model(&user).Updates(user).Error
+}
+
+func (r *userRepository) DeleteUser(uid uuid.UUID) error {
+	return r.db.Delete(&models.User{}, uid).Error
 }
