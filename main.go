37bcf13080aef0907f1f5d55acd39238ee8e8d2f
Chonlasit <chonlasit.s@arise.tech>
2023-01-12 10:44:32 +0700

    update app

--- main.go
+++ main.go
@@ -13,26 +13,27 @@ import (
 )
 
 func main() {
+
+	// database
 	db := lib.NewMySqlConnection()
-
-	_ = db
-
+	// cache
 	rdb := lib.NewRedisConnection()
 
 	userRepostory := repository.NewUserRepository(db, rdb)
 	userController := controllers.NewUserController(userRepostory)
-
-	_ = userRepostory
 
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!")
 	})
-	api := app.Group("/api")
+	// api := app.Group("/api")
 
 	//user
-	api.Get("/user", userController.GetAllUsers)
+	app.Get("/user", userController.GetAllUsers)
+	app.Post("/user/create", userController.CreateUser)
+	app.Put("/user/:id", userController.UpdateUser)
+	app.Delete("/user/:id", userController.DeleteUser)
 
 	log.Fatal(app.Listen(":4000"))
 }
