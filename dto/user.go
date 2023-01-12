ce08bb328d4ac7a4264e46041b8ce8848ff3a8a3
Chonlasit <chonlasit.s@arise.tech>
2023-01-12 10:44:04 +0700

    change dto

--- dto/user.go
+++ dto/user.go
@@ -3,10 +3,18 @@ package dto
 import "github.com/google/uuid"
 
 type User struct {
-	UserID       uuid.UUID `json:"user_id,omitempty"`
+	UserID       uuid.UUID `json:"id,omitempty"`
 	Username     string    `json:"username,omitempty"`
-	FirstName    string    `json:"first_name,omitempty"`
-	LastName     string    `json:"last_name,omitempty"`
+	FirstName    string    `json:"firstname,omitempty"`
+	LastName     string    `json:"lastname,omitempty"`
 	MobileNumber string    `json:"mobile_number,omitempty"`
 	Email        string    `json:"email,omitempty"`
+}
+
+type RequestUser struct {
+	Email     string `json:"email,omitempty"`
+	Password  string `json:"password,omitempty"`
+	FirstName string `json:"firstname,omitempty"`
+	LastName  string `json:"lastname,omitempty"`
+	Username  string `json:"username,omitempty"`
 }
