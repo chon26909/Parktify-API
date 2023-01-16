244421c516331a07e2aedd7268a7f3f17f9138a1
Chonlasit <chonlasit.s@arise.tech>
2023-01-16 13:32:30 +0700

    update auth controller

--- controllers/auth.go
+++ controllers/auth.go
@@ -3,9 +3,14 @@ package controllers
 import (
 	"fmt"
 	"parktify/dto"
+	"parktify/models"
 	"parktify/repository"
+	"parktify/utils"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type AuthController interface {
@@ -23,7 +28,49 @@ func NewAuthController(userRepo repository.UserRepository) AuthController {
 
 func (r *authController) SignUp(ctx *fiber.Ctx) error {
 
-	return nil
+	var body *dto.SignUpRequest
+	err := ctx.BodyParser(&body)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.userRepository.GetUserByEmail(body.Email)
+	if err == gorm.ErrRecordNotFound {
+
+		hashPassword, _ := utils.EncryptPassword(body.Password)
+
+		newUser := models.User{
+			UserID:       uuid.New(),
+			Username:     body.Username,
+			FirstName:    body.Firstname,
+			LastName:     body.Lastname,
+			MobileNumber: "",
+			Email:        body.Email,
+			Password:     hashPassword,
+			Created:      time.Now(),
+			Updated:      time.Now(),
+		}
+
+		err := r.userRepository.CreateUser(newUser)
+		if err != nil {
+			return err
+		}
+
+		token, err := utils.GenerateTokenAuth(newUser.UserID)
+		if err != nil {
+			fmt.Println("token error:", err)
+		}
+
+		return ctx.Status(201).JSON(fiber.Map{
+			"message": "created",
+			"token":   token,
+		})
+
+	}
+
+	return ctx.Status(400).JSON(fiber.Map{
+		"message": "Invalid username or password",
+	})
 }
 
 func (r *authController) SignIn(ctx *fiber.Ctx) error {
@@ -38,6 +85,14 @@ func (r *authController) SignIn(ctx *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
-	fmt.Print("user", user)
-	return nil
+
+	token, err := utils.GenerateTokenAuth(user.UserID)
+	if err != nil {
+		fmt.Println("token error:", err)
+	}
+
+	return ctx.Status(200).JSON(fiber.Map{
+		"message": "success",
+		"token":   token,
+	})
 }
