fff5f4afe9e8301598f3220479f80df8b84f5421
Chonlasit <chonlasit.s@arise.tech>
2023-02-02 12:29:44 +0700

    update

--- main.go
+++ main.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"log"
-	// "parktify/controllers"
-	// "parktify/lib"
-	// "parktify/middleware"
-	// "parktify/repository"
+	"parktify/controllers"
+	"parktify/lib"
+	"parktify/middleware"
+	"parktify/repository"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 
-	// // database
-	// db := lib.NewMySqlConnection()
-	// // cache
-	// rdb := lib.NewRedisConnection()
+	// database
+	db := lib.NewMySqlConnection()
+	// cache
+	rdb := lib.NewRedisConnection()
 
-	// userRepostory := repository.NewUserRepository(db, rdb)
-	// userController := controllers.NewUserController(userRepostory)
-	// authController := controllers.NewAuthController(userRepostory)
+	userRepostory := repository.NewUserRepository(db, rdb)
+	userController := controllers.NewUserController(userRepostory)
+	authController := controllers.NewAuthController(userRepostory)
 
-	// locationRepository := repository.NewLocationRepository(db, rdb)
-	// locationController := controllers.NewLocationController(locationRepository)
+	locationRepository := repository.NewLocationRepository(db, rdb)
+	locationController := controllers.NewLocationController(locationRepository)
 
 	app := fiber.New()
 
@@ -30,23 +30,23 @@ func main() {
 		return c.SendString("Hello World!")
 	})
 
-	// //auth
-	// auth := app.Group("/auth")
-	// auth.Post("/signin", authController.SignIn)
-	// auth.Post("/signup", authController.SignUp)
+	//auth
+	auth := app.Group("/auth")
+	auth.Post("/signin", authController.SignIn)
+	auth.Post("/signup", authController.SignUp)
 
-	// //user
-	// user := app.Group("/user")
-	// user.Use(middleware.AuthorizationRequired())
-	// user.Get("/", userController.GetAllUsers)
-	// user.Post("/create", userController.CreateUser)
-	// user.Put("/:id", userController.UpdateUser)
-	// user.Delete("/:id", userController.DeleteUser)
+	//user
+	user := app.Group("/user")
+	user.Use(middleware.AuthorizationRequired())
+	user.Get("/", userController.GetAllUsers)
+	user.Post("/create", userController.CreateUser)
+	user.Put("/:id", userController.UpdateUser)
+	user.Delete("/:id", userController.DeleteUser)
 
-	// //location
-	// location := app.Group("/location")
-	// location.Get("/", locationController.GetAllLocation)
-	// location.Post("/create", locationController.CreateLocation)
+	//location
+	location := app.Group("/location")
+	location.Get("/", locationController.GetAllLocation)
+	location.Post("/create", locationController.CreateLocation)
 
 	log.Fatal(app.Listen(":4000"))
 }
