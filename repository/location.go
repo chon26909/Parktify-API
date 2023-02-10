a5f2a613d4b212362401bab7110f88f7ab8a30b2
Chonlasit <chonlasit.s@arise.tech>
2023-02-10 16:24:54 +0700

    update location repo

--- repository/location.go
+++ repository/location.go
@@ -10,6 +10,7 @@ import (
 type LocationRepository interface {
 	GetAllLocation() (locations []models.Location, err error)
 	CreateLocation(location models.Location) error
+	UpdateLocation(location models.Location) error
 }
 
 type locationRepository struct {
@@ -31,7 +32,18 @@ func (r *locationRepository) GetAllLocation() (locations []models.Location, err 
 	return locations, err
 }
 
+func (r *locationRepository) GetLocationById(id string) (location *models.Location, err error) {
+
+	err = r.db.Where("id = ?", id).Find(&location).Error
+
+	return location, err
+}
+
 func (r *locationRepository) CreateLocation(location models.Location) error {
 
 	return r.db.Create(location).Error
+}
+
+func (r *locationRepository) UpdateLocation(location models.Location) error {
+	return nil
 }
